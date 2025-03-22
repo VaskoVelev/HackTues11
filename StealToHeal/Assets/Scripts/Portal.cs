4aed94a4f02b1a4aaf0bec0b78d2fd6649e2b635
Vasil Velev <vasilvelev07@gmail.com>
2025-03-22 05:20:29 +0200

    final game changes

--- StealToHeal/Assets/Scripts/Portal.cs
+++ StealToHeal/Assets/Scripts/Portal.cs
@@ -5,6 +5,6 @@ public class Portal : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
     }
 }
